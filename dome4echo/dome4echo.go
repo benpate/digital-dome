22e722168e4b6a4526669807204b33e9c0c81407
Ben Pate <ben@pate.org>
2025-01-01 14:56:50 -0700

    Fussing

--- dome4echo/dome4echo.go
+++ dome4echo/dome4echo.go
@@ -9,23 +9,22 @@ import (
 )
 
 // New returns an echo MiddlewareFunc that scans every request using Silicon Dome.
-func New(sd *dome.Dome) echo.MiddlewareFunc {
+func New(d *dome.Dome) echo.MiddlewareFunc {
 
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 
 		return func(ctx echo.Context) error {
 
 			// If this request is blocked, then halt here.
-			if err := sd.VerifyRequest(ctx.Request()); err != nil {
-				sd.HandleError(ctx.Request(), err)
+			if err := d.VerifyRequest(ctx.Request()); err != nil {
+				d.HandleError(ctx.Request(), err)
 				ctx.Response().Header().Set("X-Dome-Blocked", derp.Message(err))
 				return ctx.String(http.StatusForbidden, "Forbidden")
 			}
 
 			// Try to execute the request
 			if err := next(ctx); err != nil {
-				sd.HandleError(ctx.Request(), err)
-				return err
+				return d.HandleError(ctx.Request(), err)
 			}
 
 			// Done.
